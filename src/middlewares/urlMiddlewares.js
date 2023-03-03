a819939b68d55ec16ddd3cf123af6889fa106bd1
Gabriel castelo branco <gabriel.pdg8@gmail.com>
2023-03-03 04:20:47 -0300

    corrigido 100% pelo bot

--- src/middlewares/urlMiddlewares.js
+++ src/middlewares/urlMiddlewares.js
@@ -1,13 +1,18 @@
-import {urlSchema} from "../schemas/urlSchemas.js"
-export async function urlSchemaValidation(req, res) {
-    try {
-      const { err } = urlSchema.validate(req.body, { abortEarly: false })
+import { urlSchema } from "../schemas/urlSchemas.js";
 
-    if (err) {
-      const errs = err.details.map((detail) => detail.message);
-      return res.status(422).send({ errs });
-    }
+export async function urlSchemaValidation(req, res, next) {
+
+    try {
+      const { error } = urlSchema.validate(req.body, { abortEarly: false });
+      
+      if (error) {
+        const errorMessages = error.details.map(err => err.message);
+        return res.status(422).send(errorMessages);
+      }
    } catch (err) {
       return res.status(422).send(err.message);
     }
+    next();
+
   }
+
--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { shortenUrl } from "../controllers/urlControllers.js";
 import { authorizationValidation } from "../middlewares/authMiddlewares.js";
-
+import { urlSchemaValidation } from "../middlewares/urlMiddlewares.js";
 const urlRoutes = Router();
 
-urlRoutes.post("/urls/shorten", authorizationValidation, shortenUrl);
+urlRoutes.post("/urls/shorten", authorizationValidation, urlSchemaValidation,shortenUrl);
 
 export default urlRoutes;
