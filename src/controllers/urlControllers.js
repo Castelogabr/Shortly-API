3fc3d95a63fbb5ac34ca9f953a9e4bf3c1664c01
Gabriel castelo branco <gabriel.pdg8@gmail.com>
2023-03-03 18:02:28 -0300

    versao final

--- src/controllers/urlControllers.js
+++ src/controllers/urlControllers.js
@@ -6,21 +6,21 @@ export async function shortenUrl(req, res) {
   const session = res.locals.session;
 
   try {
-      const shortUrl = nanoid(6);
+    const shortUrl = nanoid(6);
 
-      await db.query('INSERT INTO urls (url, "shortUrl", "userId") VALUES ($1, $2, $3)', [url, shortUrl, session.userId]);
+    await db.query('INSERT INTO urls (url, "shortUrl", "userId") VALUES ($1, $2, $3)', [url, shortUrl, session.userId]);
 
-      const id = await db.query("SELECT id FROM urls WHERE url = $1", [url]);
+    const id = await db.query("SELECT id FROM urls WHERE url = $1", [url]);
 
-      return res.status(201).send({id: id.rows[0].id, shortUrl});
+    return res.status(201).send({ id: id.rows[0].id, shortUrl });
   } catch (error) {
-      return res.status(500).send(error.message);
+    return res.status(500).send(error.message);
   }
 }
 
 
 export async function getURL(req, res) {
-  const {id} = req.params;
+  const { id } = req.params;
 
   try {
     const exists = await db.query(
@@ -35,24 +35,52 @@ export async function getURL(req, res) {
     }
 
     res.status(200).send(infoUrl);
-    
+
   } catch (err) {
     return res.status(500).send(err.message);
   }
 }
 
-export async function getRedirect(req, res){
+export async function getRedirect(req, res) {
 
-  const {shortUrl} = req.params;
+  const { shortUrl } = req.params;
 
-  try{
-      const {rows: url} = await db.query('UPDATE urls SET "visitCount" = "visitCount" +1 WHERE "shortUrl"=$1 RETURNING url;', [shortUrl]);
+  try {
+    const { rows: url } = await db.query('UPDATE urls SET "visitCount" = "visitCount" +1 WHERE "shortUrl"=$1 RETURNING url;', [shortUrl]);
 
-      if(!url[0]) return res.sendStatus(404);
-      return res.redirect(url[0]);
+    if (!url[0]) return res.sendStatus(404);
+    return res.redirect(url[0]);
 
-  }catch(err){
-      return res.status(500).send(err.message);
+  } catch (err) {
+    return res.status(500).send(err.message);
   }
 }
 
+
+export async function deleteUrl(req, res) {
+
+  try {
+    const { authorization } = req.headers
+    const { id } = req.params;
+    const token = authorization?.replace("Bearer ", '')
+
+    if (!parseInt(id))
+      return res.sendStatus(404)
+
+    const userId = await db.query(`SELECT "userId" FROM sessions WHERE token = $1;`, [token]);
+    const result = await db.query(`SELECT * FROM urls WHERE id = $1;`, [id]);
+
+    if (result.rowCount === 0) return res.sendStatus(404)
+
+
+    if (result.rows[0].userId != userId.rows[0].userId)  return res.sendStatus(401)
+  
+
+    await db.query(`DELETE FROM urls WHERE id = $1;`, [id]);
+      return res.sendStatus(204)
+
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+
+}
--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { shortenUrl, getURL, getRedirect } from "../controllers/urlControllers.js";
+import { shortenUrl, getURL, getRedirect,deleteUrl } from "../controllers/urlControllers.js";
 import { authorizationValidation } from "../middlewares/authMiddlewares.js";
 import { urlSchemaValidation } from "../middlewares/urlMiddlewares.js";
 const urlRoutes = Router();
@@ -7,6 +7,7 @@ const urlRoutes = Router();
 urlRoutes.post("/urls/shorten", authorizationValidation, urlSchemaValidation,shortenUrl);
 urlRoutes.get("/urls/:id", getURL);
 urlRoutes.get('/urls/open/:shortUrl', getRedirect);
+urlRoutes.delete("/urls/:id",  deleteUrl);
 
 
 export default urlRoutes;
