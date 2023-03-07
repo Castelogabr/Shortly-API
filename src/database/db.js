b4bc939a022d4783d6de659cb25d6c344a644502
Gabriel castelo branco <gabriel.pdg8@gmail.com>
2023-03-07 13:17:38 -0300

    versao deploy

--- src/database/db.js
+++ src/database/db.js
@@ -1,10 +1,11 @@
 import pg from "pg";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const { Pool } = pg;
 
-const configDatabase = {
+export const configDatabase = {
   connectionString: process.env.DATABASE_URL,
   ...(process.env.NODE_ENV === "production" && {
     ssl: {
@@ -13,6 +14,4 @@ const configDatabase = {
   }),
 };
 
-const db = new Pool(configDatabase);
-
-export default db;
+export const db = new Pool(configDatabase);
